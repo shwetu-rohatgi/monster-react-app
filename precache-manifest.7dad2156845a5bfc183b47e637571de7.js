ada4ceeb64578e858486adf876b57a55c36da003
Shwetanshu Rohatgi <shwetanshu.rohatgi@gmail.com>
2020-03-27 21:00:14 -0700

    Updates

---
+++ precache-manifest.7dad2156845a5bfc183b47e637571de7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6446967616e357430e583b155f58f2",
+    "url": "/monster-react-app/index.html"
+  },
+  {
+    "revision": "441965bb33c96e27b739",
+    "url": "/monster-react-app/static/css/main.5cb6e852.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monster-react-app/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-react-app/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "441965bb33c96e27b739",
+    "url": "/monster-react-app/static/js/main.22215eb9.chunk.js"
+  },
+  {
+    "revision": "e8d8d402e290a7617dbb",
+    "url": "/monster-react-app/static/js/runtime-main.7fe1545e.js"
+  }
+]);
